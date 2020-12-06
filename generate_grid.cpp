60e60d12cba529dba85507ee513874150c8049aa
Aaron Nguyen <aaronnguyen.h@gmail.com>
2020-12-06 14:58:41 -0800

    generate random grid function

--- generate_grid.cpp
+++ generate_grid.cpp
@@ -5,35 +5,21 @@
 #include "generate_grid.h"
 #include <vector>
 #include <iostream>
+#include <random>
 
-generate_grid::generate_grid(
-        unordered_map<char, int> inputCellKeyVals, vector<vector<char>> inputTerrainMap) {
 
-    generate_grid::cellKeyVals = inputCellKeyVals;
-    generate_grid::terrainMapGrid = inputTerrainMap;
-    generate_grid::convertTerrainToCostGrid();
-}
+generate_grid::generate_grid(bool cinCapture) {
 
-generate_grid::generate_grid() {
-    generate_grid::input_capture();
-    generate_grid::convertTerrainToCostGrid();
-}
-
-void generate_grid::convertTerrainToCostGrid() {
-
-    int rLen = terrainMapGrid.size();
-    vector<vector<int>> costGrid(rLen, vector<int>());
-
-    for(int i = 0; i < rLen; i++){
-        vector<char> terrainRow = terrainMapGrid[i];
-        for(char cell: terrainRow){
-            auto search = cellKeyVals.find(cell);
-            costGrid[i].emplace_back(search->second);
+    if (cinCapture) {
+        input_capture();
+    }
+    else {
+        vector<int> assortedAmts = {10, 20, 50, 200, 500, 1000};
+        for(int a: assortedAmts){
+            costGrid cg = generateRandomGrid(a);
+            gridDat.assorted.insert({a, cg});
         }
     }
-
-    costMapGrid = costGrid;
-
 }
 
 void generate_grid::input_capture() {
@@ -64,27 +50,93 @@ void generate_grid::input_capture() {
         }
     }
 
-    // private:
-    //    pair<int,int> startPoint;
-    //    pair<int,int> endPoint;
     int startX, startY, endX, endY;
     cin >> startX >> startY;
     cin >> endX >> endY;
-    startPoint = make_pair(startX, startY);
-    endPoint = make_pair(endX, endY);
+
+    // format the data to be distributed.
+    costGrid cg;
+    cg.grid = convertTerrainToCostGrid(terrainMapGrid, cellKeyVals);
+    cg.rowCount = rowAmt;
+    cg.colCount = colAmt;
+    cg.start = make_pair(startX, startY);
+    cg.end = make_pair(endX, endY);
+
+    gridDat.specific = cg;
 
 }
 
-void generate_grid::display_data() {
-    cout << "Start: (" << startPoint.first << "," << startPoint.second << ")\n";
-    cout << "End: (" << endPoint.first << "," << endPoint.second << ")\n";
+gridData generate_grid::getGridData() {
 
+    cout << gridDat.assorted.size()  << "\n";
+    return gridDat;
+}
+
+costGrid generate_grid::generateRandomGrid(int amt) {
+
+    //create nxn grids with assorted numbers. going to use 2 digit numbers
+
+    random_device rd;
+    mt19937 mt(rd());
+    uniform_int_distribution dist(10, 99);
+
+    vector<vector<int>> r;
+    for (int i = 0; i < amt; ++i){
+        vector<int> t;
+        for (int j = 0; j < amt; ++j){
+            t.emplace_back(dist(mt));
+        }
+        r.emplace_back(t);
+    }
+
+    costGrid tempCG;
+    tempCG.rowCount = amt;
+    tempCG.colCount = amt;
+    tempCG.grid = r;
+
+
+    uniform_int_distribution coordDist(0, amt-1);
+    tempCG.start = make_pair(coordDist(mt), coordDist(mt));
+    tempCG.end = make_pair(coordDist(mt), coordDist(mt));
+
+    return tempCG;
+}
+
+vector<vector<int>>
+convertTerrainToCostGrid(vector<vector<char>> &terrainMapGrid,
+                         unordered_map<char, int> &cellKeyVals) {
+
+    int rLen = terrainMapGrid.size();
+    vector<vector<int>> costGrid(rLen, vector<int>());
+
+    for(int i = 0; i < rLen; i++){
+        vector<char> terrainRow = terrainMapGrid[i];
+        for(char cell: terrainRow){
+            auto search = cellKeyVals.find(cell);
+            costGrid[i].emplace_back(search->second);
+        }
+    }
+
+    return costGrid;
+}
+
+void display_data(
+        pair<int,int> start, pair<int,int> end,
+        pair<int, int> dimensions, vector<vector<int>> cg) {
+    cout << "Start: (" << start.first << "," << start.second << ")\n";
+    cout << "End: (" << end.first << "," << end.second << ")\n";
+
+    cout << "Dimensions: R" << dimensions.first << " x C" << dimensions.second << "\n";
     cout << "TerrainMap in Cost form: \n";
-    for(auto i: costMapGrid){
-        for(auto cell: i){
+    for(auto row: cg){
+        for(auto cell: row){
             cout << cell << " ";
         }
         cout << "\n";
     }
 
+}
+
+void display_data(costGrid cg) {
+    display_data(cg.start, cg.end, make_pair(cg.rowCount, cg.colCount), cg.grid);
 }
